38642e2df6e3a131132325b0ac661d315916a67a
Jason IBISI <jasonibisi@gmail.com>
2019-12-26 00:04:49 +0100

    User Login

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,6 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
 
-const Login = () => {
+const Login = props => {
+  const authContext = useContext(AuthContext);
+  const alertContext = useContext(AlertContext);
+
+  const { setAlert } = alertContext;
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push('/');
+    }
+
+    if (error) {
+      setAlert(error, 'danger');
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
   const [user, setUser] = useState({
     email: '',
     password: ''
@@ -15,7 +35,14 @@ const Login = () => {
   const onSubmit = e => {
     e.preventDefault();
 
-    console.log('Login Submit');
+    if (email === '' || password === '') {
+      setAlert('Please fill in all fields');
+    } else {
+      login({
+        email,
+        password
+      });
+    }
   };
 
   return (
@@ -26,7 +53,13 @@ const Login = () => {
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" value={email} onChange={onChange} />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
         </div>
         <div className="form-group">
           <label htmlFor="paswword">Password</label>
@@ -35,6 +68,7 @@ const Login = () => {
             name="password"
             value={password}
             onChange={onChange}
+            required
           />
         </div>
         <input
